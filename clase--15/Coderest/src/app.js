3f91b31055985b47ce5625614ab7f47063d19338
stocaimaza <stocaimaza@hotmail.com>
2024-04-16 11:47:27 -0300

    yo tomo licor yo tomo cerveza

--- clase--15/Coderest/src/app.js
+++ clase--15/Coderest/src/app.js
@@ -16,12 +16,30 @@ import express from "express";
 const app = express(); 
 const PUERTO = 8080;
 import imagenRouter from "./routes/imagen.router.js";
+import multer from "multer";
 import "./database.js";
+import exphbs from "express-handlebars";
 
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
 app.use(express.static("./src/public"));
+//Configuramos Multer: 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./src/public/img");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+app.use(multer({storage}).single("image"));
+
+//Express-Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
 
 //Rutas 
 app.use("/", imagenRouter);
--- clase--15/Coderest/src/routes/imagen.router.js
+++ clase--15/Coderest/src/routes/imagen.router.js
@@ -1,8 +1,47 @@
 import {Router} from "express";
+import ImagenModel from "../models/imagen.model.js";
+import {promises as fs} from "fs";
 const router = Router(); 
 
-router.get("/", (req, res) => {
-    res.send("Olis, como estan?");
+//Ruta a la raíz de la aplicación
+
+router.get("/", async (req, res) => {
+    const imagenes = await ImagenModel.find().lean();
+    res.render("index", {imagenes});
+})
+
+//Ruta para acceder al formulario de carga de imagenes: 
+
+router.get("/upload", (req, res) => {
+    res.render("upload");
+})
+
+//Ruta upload para cargar imagenes: 
+
+router.post("/upload",  async (req, res) => {
+    try {
+        const imagen = new ImagenModel(); 
+        imagen.title = req.body.title;
+        imagen.description = req.body.description;
+        imagen.filename = req.file.filename;
+        imagen.path = "/img/" + req.file.filename;
+
+        //Guardamos la imagen en la base de datos
+        await imagen.save(); 
+
+        res.redirect("/");
+    } catch (error) {
+        res.status(500).send({message: "Error en el servidor, vamos a morir"});
+    }
+})
+
+//Ruta para eliminar una imagen
+
+router.get("/image/:id/delete", async (req, res) => {
+    const {id} = req.params; 
+    const imagen = await ImagenModel.findByIdAndDelete(id);
+    await fs.unlink("./src/public" + imagen.path);
+    res.redirect("/");
 })
 
 
